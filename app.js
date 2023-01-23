652e290e0614426b71bee602ccab419144edef02
AbhishekManra <abhishek.manral2021@vitbhopal.ac.in>
2023-01-24 01:30:42 +0530

    tonight's commit

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const items = [];
 const posts = [];
 app.set('view engine', 'ejs');
 
@@ -37,8 +38,8 @@ app.get("/posts/:topic",(req,res)=>{
   });
 });
 app.post("/",(req,res)=>{
-  const a = req.body.tital;
-  const b = req.body.post;
+  const a = req.body.name_inp;
+  const b = items;
   var obj = {
     title : a,
     content : b
@@ -46,6 +47,17 @@ app.post("/",(req,res)=>{
   posts.push(obj);
   res.redirect("/");
 });
+
+                                // Second half of the Scene Starts from here
+app.get("/todopost",(req,res)=>{
+  res.render("list2",{listTitle: "Your To-Do List", newListItems: items});
+});
+app.post("/todopost",(req,res)=>{
+  var newitem = req.body.newItem;
+  items.push(newitem);
+  res.redirect("/todopost");
+});
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
